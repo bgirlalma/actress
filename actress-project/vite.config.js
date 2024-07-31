fd4a8be0dfc1019d675d0916e3a1aef00a90021f
Alina <akulina.alina1999gmail.com>
2024-07-31 17:02:58 +0300

    fix

---
+++ actress-project/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), svgr()],
+    resolve: {
+        alias: {
+            src: './src',
+            components: './src/Components',
+            pages: './src/Pages'
+        },
+    },
+    base: '/actress/'
+});
