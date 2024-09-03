90da97ca827e1da5d03d7d292973706703942452
Alina <akulina.alina1999gmail.com>
2024-09-03 12:51:21 +0300

    Updates

---
+++ assets/PhotoPage-VNz7tx94.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DQXqQcNN.js";const r=()=>t.jsx("div",{children:t.jsx("h2",{children:"List"})}),o=()=>t.jsx("div",{children:t.jsx(r,{})});export{o as default};
---
+++ assets/VideoPage-DQ-RuuzJ.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DQXqQcNN.js";const r=()=>e.jsx("div",{children:e.jsx("h2",{children:"Video"})}),s=()=>e.jsx("div",{children:e.jsx(r,{})});export{s as default};
